05cad44c53a8281cbdf2e9dce07a4eecb2e413f4
Billel <billel@MBP-de-Billel.numericable.fr>
2023-06-08 22:45:05 +0200

    count monkey enhanced code es6

--- count-monkeys.js
+++ count-monkeys.js
@@ -1,8 +1,9 @@
 function monkeyCount(n) {
-  let arr = [];
-  for (i = 1; i <= n; i++) {
-    arr.push(i);
-  }
-  return arr;
+  // let arr = [];
+  // for (i = 1; i <= n; i++) {
+  //   arr.push(i);
+  // }
+  // return arr;
+  return Array.from({ length: n }, (e, i) => i + 1);
 }
 console.log(monkeyCount(78));
