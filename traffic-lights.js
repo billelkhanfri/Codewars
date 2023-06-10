01992b8e558d6afc0a5081dba57c3387188efca6
billelkhanfr <khanfri.billel@yahoo.fr>
2023-06-10 16:57:59 +0200

    practice objects

---
+++ traffic-lights.js
@@ -0,0 +1,11 @@
+function updateLight(current) {
+  //   let arr = ["green", "yellow", "red"];
+  //   let index = arr.findIndex((e) => e === current);
+  //   return current === arr[arr.length - 1] ? arr[0] : arr[index + 1];
+  return {
+    green: "yellow",
+    yellow: "red",
+    red: "green",
+  }[current];
+}
+console.log(updateLight("green"));
